3ef393ed70ee2cd2c06911f86c8db4900d65a044
Muhammad Muneeb <safdarmuneeb@gmail.com>
2023-12-11 18:50:03 +0500

    'changing'

--- src/pages/Doctor/Appointment.js
+++ src/pages/Doctor/Appointment.js
@@ -2,8 +2,79 @@ import React from 'react'
 
 export default function Appointment() {
   return (
-    <main>
-    <div>Appointment</div>
+    <main className='container-fluid'>
+      <div className="container">
+
+        <h2 className='text-center mb-5'>Today Appointment</h2>
+        <div className="row mt-2">
+          <div className="col">
+            <div className="table-responsive">
+              <table className="table table-striped">
+                <thead>
+                  <tr className="tr">
+                    <th>#</th>
+                    <th>Name</th>
+                    <th>PhoneNo</th>
+                    <th>SlotNo</th>
+                    <th>Reports</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td>Ali</td>
+                    <td>03137832982</td>
+                    <td>4</td>
+                    <td>head pain</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td>bilal</td>
+                    <td>03675832982</td>
+                    <td>5</td>
+                    <td>hair</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td>Ahamd</td>
+                    <td>03137832982</td>
+                    <td>6</td>
+                    <td>head</td>
+                  </tr>
+                  <tr>
+                    <td>4</td>
+                    <td>Akmal</td>
+                    <td>03137832982</td>
+                    <td>7</td>
+                    <td>pain</td>
+                  </tr>
+                  <tr>
+                    <td>5</td>
+                    <td>usman</td>
+                    <td>0353532982</td>
+                    <td>8</td>
+                    <td>legs pain</td>
+                  </tr>
+                  <tr>
+                    <td>6</td>
+                    <td>zeerak</td>
+                    <td>0333532982</td>
+                    <td>9</td>
+                    <td>gala</td>
+                  </tr>
+                  <tr>
+                    <td>7</td>
+                    <td>Amjad</td>
+                    <td>03137832982</td>
+                    <td>10</td>
+                    <td>head pain</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
--- src/pages/Doctor/Home.js
+++ src/pages/Doctor/Home.js
@@ -5,7 +5,7 @@ export default function stickyWall() {
   return (
     <main className="container-fluid bg-dark">
       <div className="container">
-        <div className="row">
+        <div className="row mt-3">
           <div className="col">
             <div className="card p-2 p-md-3 p-lg-4">
               <h3 className='text-center'>Schedule Details</h3>
@@ -46,24 +46,24 @@ export default function stickyWall() {
           </div>
           <div className="col">
             <div className="card p-2 p-md-3 p-lg-4">
-              <h3 className='text-center'>Appointment Details</h3>
-              <div className="row">
+              <h3 className='text-center mb-4'>Appointment Details</h3>
+              <div className="row mt-2">
                 <div className="col">Appointment</div>
                 <div className="col">Patients</div>
               </div>
-              <div className="row">
+              <div className="row  mt-1">
                 <div className="col">Today</div>
                 <div className="col">30</div>
               </div>
-              <div className="row">
+              <div className="row  mt-1">
                 <div className="col">Tomarrow</div>
                 <div className="col">40</div>
               </div>
-              <div className="row">
+              <div className="row  mt-1">
                 <div className="col">Total</div>
                 <div className="col">480</div>
               </div>
-              <div className="row mt-3">
+              <div className="row mt-5">
                 <div className="col text-center">
                   <Link className='btn btn-outline-primary' to='/Appointment'>Manage Appointment</Link>
                   </div>
@@ -71,6 +71,75 @@ export default function stickyWall() {
             </div>
           </div>
         </div>
+        <h2 className='text-white text-center mt-4'>Total Patient</h2>
+        <div className="row mt-2">
+                <div className="col">
+                    <div className="table-responsive">
+                        <table className="table table-striped">
+                            <thead>
+                                <tr className="tr">
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>PhoneNo</th>
+                                    <th>SlotNo</th>
+                                    <th>Reports</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                  <td>1</td>
+                                  <td>Ali</td>
+                                  <td>03137832982</td>
+                                  <td>4</td>
+                                  <td>head pain</td>
+                                </tr>
+                                <tr>
+                                  <td>2</td>
+                                  <td>bilal</td>
+                                  <td>03675832982</td>
+                                  <td>5</td>
+                                  <td>hair</td>
+                                </tr>
+                                <tr>
+                                  <td>3</td>
+                                  <td>Ahamd</td>
+                                  <td>03137832982</td>
+                                  <td>6</td>
+                                  <td>head</td>
+                                </tr>
+                                <tr>
+                                  <td>4</td>
+                                  <td>Akmal</td>
+                                  <td>03137832982</td>
+                                  <td>7</td>
+                                  <td>pain</td>
+                                </tr>
+                                <tr>
+                                  <td>5</td>
+                                  <td>usman</td>
+                                  <td>0353532982</td>
+                                  <td>8</td>
+                                  <td>legs pain</td>
+                                </tr>
+                                <tr>
+                                  <td>6</td>
+                                  <td>zeerak</td>
+                                  <td>0333532982</td>
+                                  <td>9</td>
+                                  <td>gala</td>
+                                </tr>
+                                <tr>
+                                  <td>7</td>
+                                  <td>Amjad</td>
+                                  <td>03137832982</td>
+                                  <td>10</td>
+                                  <td>head pain</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
       </div>
     </main>
   )
